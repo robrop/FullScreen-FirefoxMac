8af18166f9c4009b8679f59d2e50bb0418ff27d4
robrop <robert.roppel@web.de>
2020-08-28 18:26:02 +0200

    Added js and css. Updated readme

--- README.md
+++ README.md
@@ -5,10 +5,22 @@
 
 ### Dependency
 
-Install userChromeJS:   
+Install userChromeJS:    
 https://github.com/xiaoxiaoflood/firefox-scripts
 
+## Install full screen script
+Place both file in your profile folder.    
+Path:
+`/profile-folder/chrome/`
 
-### Install path
+## Customisation
 
+If you have the bookmark-bar enabled change **line 15** in `userChromeFullScreen.css` to    
+```css
+margin-top: calc(-68px - var(--tab-min-height));
+```
+
+<!--
+Install path for js injection
 https://developer.mozilla.org/en-US/docs/Mozilla/Firefox/Enterprise_deployment_before_60#Configuration
+-->
---
+++ chrome/MacFullScreen.uc.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name            MacFullScreen
+// @author          rojaro
+// @include         main
+// @startup         UC.FullScreen.exec(win);
+// @shutdown        UC.FullScreen.destroy();
+// @onlyonce
+// ==/UserScript==
+
+
+UC.FullScreen = {
+	exec: function(win) {
+		let document = win.document;
+		let gBrowser = win.gBrowser;
+
+		var ios = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
+		var ds = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties);
+
+		// Get the chrome directory in the current profile
+		var chromepath = ds.get("UChrm", Ci.nsIFile);
+		chromepath.append("userChromeFullScreen.css");
+		UC.FullScreen.chromefile = ios.newFileURI(chromepath);
+
+		let keyset = _uc.createElement(document, 'keyset', {
+			id: 'FullScreen-keyset'
+		});
+		document.getElementById('mainKeyset').insertAdjacentElement('afterend', keyset);
+
+		let FullScreenKey = _uc.createElement(document, 'key', {
+			id: 'FullScreen-key',
+			modifiers: 'accel,control,shift',
+			key: 'f',
+			oncommand: 'UC.FullScreen.toggleFullScreen()',
+		});
+		keyset.appendChild(fullkey);
+	},
+
+	toggleFullScreen() {
+		var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
+
+		if (sss.sheetRegistered(UC.FullScreen.chromefile, sss.USER_SHEET)) {
+			sss.unregisterSheet(UC.FullScreen.chromefile, sss.USER_SHEET);
+		} else {
+			sss.loadAndRegisterSheet(UC.FullScreen.chromefile, sss.USER_SHEET);
+		}
+	},
+
+	destroy: function() {
+		_uc.windows((doc, win) => {
+			doc.getElementById('FullScreen-keyset').remove();
+		});
+		delete UC.FullScreen;
+	}
+}
